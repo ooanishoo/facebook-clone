b96ee9db01e8b02d468039a58bfa0e927e629e92
ooanishoo <anish.maharjan12341@gmail.com>
2019-06-28 08:26:46 +1000

    added more global variables for image

--- lib/models/global.dart
+++ lib/models/global.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 String userProfileImage = 'https://www.thispersondoesnotexist.com/image';
 String userStoryCoverImage = 'https://source.unsplash.com/random/';
 
-
+String myStoryImage = 'https://picsum.photos/200/300';
