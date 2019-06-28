55bd1b4bd4529a2ea7775e3547af359d5ef93304
ooanishoo <anish.maharjan12341@gmail.com>
2019-06-28 15:51:32 +1000

    added user class

---
+++ lib/models/user.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class User{
+  String firstname;
+  String lastname;
+  Image profilePicture;
+  bool hasStory;
+  List<User> friends;
+  List<Post> savedPosts;
+
+  User(
+    this.firstname,
+    this.lastname,
+    this.profilePicture,
+    this.hasStory,
+    this.friends,
+    this.savedPosts
+  );
+
+}
