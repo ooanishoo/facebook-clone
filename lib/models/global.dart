e36b55dd12628b2f4ae89ee9db8a6d9574c57332
ooanishoo <anish.maharjan12341@gmail.com>
2019-07-01 09:07:40 +1000

    chnage the url for profile image

--- lib/models/global.dart
+++ lib/models/global.dart
@@ -5,7 +5,7 @@ import 'comment.dart';
 import 'post.dart';
 
 String userProfileImage = 'https://www.thispersondoesnotexist.com/image';
-String userStoryCoverImage = 'https://source.unsplash.com/random/';
+String userStoryCoverImage = 'https://picsum.photos/200/300';
 
 String myStoryImage = 'https://picsum.photos/200/300';
 
