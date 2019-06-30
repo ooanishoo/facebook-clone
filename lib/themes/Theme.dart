015c77d46495c521a71bb97f8326e69dbb02f6e6
ooanishoo <anish.maharjan12341@gmail.com>
2019-07-01 00:01:19 +1000

    implemented ThemeData to seperate default themes for the app

---
+++ lib/themes/Theme.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+Object MyTheme() {
+  return ThemeData(
+    primaryColor: Color(0xff1777F0),
+    textTheme: TextTheme(
+      title: TextStyle(
+        color: Color(0xff1D1E22),
+        fontSize: 26,
+        fontWeight: FontWeight.w600
+      ),
+      subhead: TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+      )
+    )
+  );
+}
